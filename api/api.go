e30641d3136f6a9b9b59aac2bec92703ba2ae438
infernalfire72 <julius.tzunami@yahoo.de>
2020-06-13 16:15:55 +0200

    oops

--- api/api.go
+++ api/api.go
@@ -18,22 +18,8 @@ func Start() {
 	r.GET("/", MemHandler)
 	r.GET("/beatmap/", BeatmapHandler)
 	r.GET("/leaderboard/", LeaderboardHandler)
-	r.GET("/rm", Rm)
-	r.GET("/ad", Ad)
 	log.Info("Starting API")
 	log.Error(fasthttp.ListenAndServe(":5000", r.Handler))
-}
-
-func Rm(ctx *fasthttp.RequestCtx) {
-	id := ctx.QueryArgs().GetUintOrZero("i")
-
-	leaderboards.Cache.RemoveUser(id)
-}
-
-func Ad(ctx *fasthttp.RequestCtx) {
-	id := ctx.QueryArgs().GetUintOrZero("i")
-
-	leaderboards.Cache.AddUser(id)
 }
 
 func MemHandler(ctx *fasthttp.RequestCtx) {
